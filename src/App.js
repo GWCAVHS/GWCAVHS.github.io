263d32d44c0c22dd45e7f68c73a8a7a33112fcdd
Alice <879190@apps.district196.org>
2025-02-21 15:55:17 -0600

    Grade header
    Working on grade header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // add styles from App.css
 import './App.css';
+import GradeHeader from './components/grade-header/grade-header';
 
 function App() {
   return (
     <div className="App">
         <h1>hello world</h1>
+        <GradeHeader grade="9"></GradeHeader>
+        <GradeHeader grade="10"></GradeHeader>
+        <GradeHeader grade="11"></GradeHeader>
+        <GradeHeader grade="12"></GradeHeader>
     </div>
+    
   );
 }
 
