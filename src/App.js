b6356d26cfe25e79bc79e70532bbfe11f24e1c3d
India44 <ijbrutten@gmail.com>
2025-04-11 16:57:59 -0500

    Modal
    Modal for course descriptions, prerequisites, and concurrent enrollment.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 // add styles from App.css
 import './App.css';
+import GradeDisplay from './components/GradeDisplay/GradeDisplay';
+import CourseDescriptionModal from './components/Modal/CourseDescription';
 
 function App() {
   return (
     <div className="App">
         <h1>hello world</h1>
+        <GradeDisplay grade={9} />
+        <GradeDisplay grade={10} />
+        <GradeDisplay grade={11} />
+        <GradeDisplay grade={12} />
+
+        <br />
+        <br />
+        <CourseDescriptionModal
+          courseName="Math 101"
+          credits={3}
+          description="This ia a math class"
+          prereq="na"
+          concurrent="na"
+        />
     </div>
   );
 }
