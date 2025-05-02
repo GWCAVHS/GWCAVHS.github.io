695918fc3b416da5eded12885e73ac1a58b80652
tessamyers <tessa5jmyers@gmail.com>
2025-05-02 16:28:22 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import CourseDescriptionModal from './components/Modal/CourseDescription';
 function App() {
   return (
     <div className="App">
+        <AppTitle title={'PLANN IT'} />
         <MasterAccordian subject={'English'} courseDescription={"English 101, English 102"} /> 
         <MasterAccordian subject={'Math'} courseDescription={"Math 101, Math 102"} /> 
         <MasterAccordian subject={'Science'} courseDescription={"Science 101, Science 102"} /> 
         <MasterAccordian subject={'Social Studies'} courseDescription={"Social Studies 101, Social Studies 102"} />
         <MasterAccordian subject={'Electives'} courseDescription={"Electives 101, Electives 102"} />  
-        <AppTitle title={'PLANN IT'} />
         <GradeDisplay grade={"Grade 9"} />
         <GradeDisplay grade={"Grade 10"} />
         <GradeDisplay grade={"Grade 11"} />
